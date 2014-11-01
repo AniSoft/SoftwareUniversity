cac9a98ccc321cf27de663d649b4f45eae704be4
Anita <AniSoft@users.noreply.github.com>
2014-11-01 18:57:29 +0200

    Create Players.cs

---
+++ Unity3D/01.UnityBasics-HW/Players.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float velocity;
+
+    public void Start()
+    {
+        velocity = 4;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(-velocity, 0, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(velocity, 0, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(0, 0, -velocity) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(0, 0, velocity) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(0, velocity, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(new Vector3(0, -velocity, 0) * Time.deltaTime);
+        }
+    }
+}
